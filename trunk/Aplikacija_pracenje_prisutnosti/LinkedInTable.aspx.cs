9e4b360da0a057c2cc0168a5d99d46477b6704e1
mario.volarevic@gmail.com <mario.volarevic@gmail.com@f3d8bd82-491a-7bc4-cf49-62fd96969986>
2011-12-14 00:57:49 +0000

    - stavljena stara verzija za linked in koja je radila, bez iframe redirecta

--- trunk/Aplikacija_pracenje_prisutnosti/LinkedInTable.aspx.cs
+++ trunk/Aplikacija_pracenje_prisutnosti/LinkedInTable.aspx.cs
@@ -19,7 +19,6 @@ public partial class _Default : System.Web.UI.Page
     //private OAuthLinkedIn _oauth = new OAuthLinkedIn();
     protected void Page_Load(object sender, EventArgs e)
     {
-        
         string previousUrl="";
         if(Request.UrlReferrer!=null)
             previousUrl=Request.UrlReferrer.AbsolutePath;
@@ -59,7 +58,6 @@ public partial class _Default : System.Web.UI.Page
                 ListBox1.Items.Add(Status(osobe.ElementAt(i).name, osobe.ElementAt(i).surname));
             }
         }
-        IframeHelper.IframeRedirect("LinkedInTable.aspx", true, true);
 
     }
 
